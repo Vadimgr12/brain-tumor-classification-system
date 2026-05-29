482f8f6ece087f59e3fcfe6f659786853129c81a
Vadim Griaznov <vadimgriaznov@MacBook-Air-Vadim.local>
2026-05-29 22:26:08 +0300

    Add inference latency tracking

--- services/triton_client.py
+++ services/triton_client.py
@@ -3,11 +3,13 @@ from services.postprocessing import postprocess
 import tritonclient.http as httpclient
 from fastapi import HTTPException
 from services.constants import INPUT_TENSOR_NAME, OUTPUT_TENSOR_NAME
+import time
 
 client = httpclient.InferenceServerClient(url="localhost:8000")
 
 
 def infer(x):
+    start = time.time()
     try:
         x = preprocess(x)
 
@@ -27,20 +29,18 @@ def infer(x):
             outputs=[requested_output],
         )
         logits = result.as_numpy(OUTPUT_TENSOR_NAME)
+
         if logits is None:
             raise RuntimeError("Triton returned empty output")
 
     except Exception as e:
-        raise HTTPException(
-            status_code=503, detail=f"TRITON MODEL ERROR. Preprocessing error: {str(e)}"
-        )
+        raise HTTPException(status_code=503, detail=f"TRITON MODEL ERROR: {str(e)}")
     try:
         answer = postprocess(logits)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"POSTPROCESS ERROR: {str(e)}")
 
+    end = time.time()
+    latency = end - start
+    answer["end_to_end_latency_s"] = latency
     return answer
-
-
-if __name__ == "__main__":
-    infer(input)
